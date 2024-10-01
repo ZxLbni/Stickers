0ed24c29228c279a92e81e5286fe34c2866c522e
Labani Behera 🎯 <158763270+Mrlabani@users.noreply.github.com>
2024-10-01 18:12:22 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    token: 'YOUR_TELEGRAM_BOT_TOKEN' // Replace with your bot's token
+    token: '7334921772:AAHvmMMCCjmLPu1ZfDQsyq4JYMKz94x-3NE' // Replace with your bot's token
 };
